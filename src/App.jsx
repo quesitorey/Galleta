f7010a25036a6f2c4186722cf6308ee4ff1000b5
quesitorey <galleta_chips@hotmail.com>
2023-05-16 15:50:29 -0500

    galleta final final

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import "/src/phrases.json"
+import dataa from "/src/phrases.json"
 function App() {
 
   const [ index, setIndex ] = useState(0)
   const [ phrase, setPhrase ] = useState([])
   const images = [ "/imagenes/fondo1.jpg", "/imagenes/fondo2.jpg", "/imagenes/fondo3.jpg", "/imagenes/fondo4.jpg" ]
 
-  let phrasess = './src/phrases.json'
   let data;
   const randomNum = () => Math.floor(Math.random() * data.length ) + 1;
 
@@ -27,7 +26,7 @@ function App() {
 
   async function getPhrase() {
     try {
-      const res = await fetch(phrasess)
+      const res = await fetch(dataa)
       data = await res.json()
       setPhrase(data[randomNum()])
     } catch (err) {
